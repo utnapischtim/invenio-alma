a003e8cfbfd0cc153aead1115f03f9ace1c184c5
Philipp Gualdi <philipp.gualdi@tugraz.at>
2022-04-27 15:28:46 +0200

    modification: config variables
    init config variables for alma rest api

--- invenio_alma/config.py
+++ invenio_alma/config.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021 Graz University of Technology.
+# Copyright (C) 2021-2022 Graz University of Technology.
 #
 # invenio-alma is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Invenio module to connect InvenioRDM to Alma."""
 
-INVENIO_ALMA_DEFAULT_VALUE = "foobar"
-"""Default value for the application."""
+INVENIO_ALMA_API_KEY = ""
+"""Default value for the Alma API key.
+    NZ & IZ linked records needs special permissions and only 98X fields can be changed.
+"""
+
+INVENIO_ALMA_API_HOST = "api-eu.hosted.exlibrisgroup.com"
+"""Default value for the Alma API host."""

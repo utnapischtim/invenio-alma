64e9e859f734c1efff9e0a789c81191da158f22a
Philipp Gualdi <philipp.gualdi@tugraz.at>
2022-04-27 15:29:07 +0200

    feature: config class for service
    config class for record service storing variables
    and build api request urls

---
+++ invenio_alma/services/config/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 Graz University of Technology.
+#
+# invenio-alma is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio service to connect InvenioRDM to Alma."""
+
+from .config import AlmaServiceConfig
+
+__all__ = ("AlmaServiceConfig",)

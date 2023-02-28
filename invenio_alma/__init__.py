9c83647b7eeb480283d085bdb50aa244b8346b66
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-02-28 10:49:02 +0100

    global: add services to export

--- invenio_alma/__init__.py
+++ invenio_alma/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2022 Graz University of Technology.
+# Copyright (C) 2021-2023 Graz University of Technology.
 #
 # invenio-alma is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -8,7 +8,8 @@
 """Invenio module to connect InvenioRDM to Alma."""
 
 from .ext import InvenioAlma
+from .services import AlmaRESTService, AlmaSRUService
 
 __version__ = "0.7.6"
 
-__all__ = ("__version__", "InvenioAlma")
+__all__ = ("__version__", "InvenioAlma", "AlmaSRUService", "AlmaRESTService")

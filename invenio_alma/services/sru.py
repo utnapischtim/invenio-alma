5fe6524caa925f7ac7fa78dd7df4899c15f19569
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-09-08 15:55:55 +0200

    ruff: update

--- invenio_alma/services/sru.py
+++ invenio_alma/services/sru.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2023 Graz University of Technology.
 #
 # invenio-alma is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 
 """Alma SRU Service."""
+
+
+from __future__ import annotations
+
 from xml.etree.ElementTree import Element
 
 from .base import AlmaAPIBase

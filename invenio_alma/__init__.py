3dd319a9c69e5d1b6cf88e17d42582ccd065024e
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-09-12 11:49:36 +0200

    release v0.10.0

--- invenio_alma/__init__.py
+++ invenio_alma/__init__.py
@@ -10,6 +10,6 @@
 from .ext import InvenioAlma
 from .services import AlmaRESTService, AlmaSRUService
 
-__version__ = "0.9.1"
+__version__ = "0.10.0"
 
 __all__ = ("__version__", "InvenioAlma", "AlmaSRUService", "AlmaRESTService")
